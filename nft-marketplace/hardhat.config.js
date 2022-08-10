c11e1e2db385ed21c0466f2a7a8b70df54722011
jhin93 <scene1993@gmail.com>
2022-08-10 16:01:08 +0900

    중간에 업데이트하는 과정이 많아서 그냥 다운로드

--- nft-marketplace/hardhat.config.js
+++ nft-marketplace/hardhat.config.js
@@ -1,23 +1,36 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
-
-const fs = require("fs")
-const privateKey = fs.readFileSync(".secret").toString()
+require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 module.exports = {
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 1337
     },
+    /*
     mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${process.env.MUMBAI_projectId}`,
-      accounts: [privateKey]
+      // Infura
+      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [process.env.privateKey]
     },
-    mainnet: {
-      url: `https://polygon-mumbai.infura.io/v3/${process.env.MAINNET_projectId}`,
-      accounts: [privateKey]
+    matic: {
+      // Infura
+      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts: [process.env.privateKey]
     }
+    */
   },
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
+
