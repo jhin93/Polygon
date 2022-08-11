702606206418a8d95ab6f0c2d890bfec7dd71282
jhin93 <scene1993@gmail.com>
2022-08-11 11:20:23 +0900

    중간커밋

---
+++ nft-marketplace/hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const infuraId = "cbc805d0b98445f689621e33aa60220f"
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    // mumbai: {
+    //   // Infura
+    //   url: `https://polygon-mumbai.infura.io/v3/${infuraId}`,
+    //   accounts: [process.env.privateKey]
+    // },
+    // matic: {
+    //   // Infura
+    //   url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+    //   accounts: [process.env.privateKey]
+    // }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
