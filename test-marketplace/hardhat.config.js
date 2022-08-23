5956f7fc5b8a1bdede9aa6b5ed034771a53014e0
jhin93 <scene1993@gmail.com>
2022-08-23 11:08:48 +0900

    컨트랙트 작성

--- test-marketplace/hardhat.config.js
+++ test-marketplace/hardhat.config.js
@@ -1,6 +1,25 @@
-require("@nomicfoundation/hardhat-toolbox");
+/* hardhat.config.js */
+require("@nomiclabs/hardhat-waffle")
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
-};
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+//  unused configuration commented out for now
+//  mumbai: {
+//    url: "https://rpc-mumbai.maticvigil.com",
+//    accounts: [process.env.privateKey]
+//  }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
